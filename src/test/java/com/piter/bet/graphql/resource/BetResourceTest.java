ea270bcacb1d652bf1292f99d23a222270693257
PiotrMichalowski96 <piotr5.12.13@gmail.com>
2023-02-21 14:46:06 +0100

    Add test scenarios for fetching bets by filters

--- src/test/java/com/piter/bet/graphql/resource/BetResourceTest.java
+++ src/test/java/com/piter/bet/graphql/resource/BetResourceTest.java
@@ -8,8 +8,13 @@ import com.piter.bet.graphql.domain.Match;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 @QuarkusTest
 class BetResourceTest {
@@ -77,6 +82,104 @@ class BetResourceTest {
 
     //then
     assertThat(bet).usingRecursiveComparison().isEqualTo(expectedBet);
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideRequestAndExpectedBets")
+  void shouldFindBetsFilteringByAmount(String request, List<Bet> expectedBets) {
+    //given input
+
+    //when
+    final List<Bet> bets = send(request)
+        .jsonPath()
+        .getList("data.betsByFilter", Bet.class);
+
+    //then
+    assertThat(bets).hasSameElementsAs(expectedBets);
+  }
+
+  private static Stream<Arguments> provideRequestAndExpectedBets() {
+    final var requestFilterByAmount = """
+        {
+          "query": "{
+             betsByFilter(filter: {
+               betAmount: {
+                 operator: \\\"gt\\\",
+                 value: \\\"900\\\"
+               }
+             }) {
+               id
+               homeTeamGoalsPred
+               awayTeamGoalsPred
+               match {
+                 id
+                 homeTeam
+                 awayTeam
+               }
+             }
+           }"
+        }
+        """;
+
+    final var requestFilterByCorrectAndAmount = """
+        {
+          "query": "{
+             betsByFilter(filter: {
+               betAmount: {
+                 operator: \\\"lt\\\",
+                 value: \\\"100\\\"
+               },
+               correct: {
+                 value: true
+               }
+             }) {
+               id
+               homeTeamGoalsPred
+               awayTeamGoalsPred
+               correct
+               betAmount
+               match {
+                 id
+                 homeTeam
+                 awayTeam
+               }
+             }
+           }"
+        }
+        """;
+
+    final var expectedBetsFilterByAmount = List.of(
+        Bet.builder()
+            .id(18L)
+            .homeTeamGoalsPred(0)
+            .awayTeamGoalsPred(4)
+            .match(Match.builder()
+                .id(8L)
+                .homeTeam("Almeria")
+                .awayTeam("FC Barcelona")
+                .build())
+            .build()
+    );
+
+    final var expectedBetsFilterByCorrectAndAmount = List.of(
+        Bet.builder()
+            .id(17L)
+            .homeTeamGoalsPred(0)
+            .awayTeamGoalsPred(5)
+            .correct(true)
+            .betAmount(BigDecimal.valueOf(90L))
+            .match(Match.builder()
+                .id(8L)
+                .homeTeam("Almeria")
+                .awayTeam("FC Barcelona")
+                .build())
+            .build()
+    );
+
+    return Stream.of(
+        Arguments.of(requestFilterByAmount, expectedBetsFilterByAmount),
+        Arguments.of(requestFilterByCorrectAndAmount, expectedBetsFilterByCorrectAndAmount)
+    );
   }
 
   private Response send(String request) {
